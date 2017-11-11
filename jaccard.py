d9526421441d289a0cc0284a75866b3633f04ccb
Alexander Olmedo <olmedo2408@gmail.com>
2017-11-10 21:09:58 -0500

    Guardar combinaciones en archivo soluciones

--- jaccard.py
+++ jaccard.py
@@ -1,15 +1,16 @@
 import csv
 
-with open('ZINC_chemicals_test.tsv','rb') as tsvIn:
+with open('ZINC_chemicals.tsv','rb') as tsvIn, open('solution_jaccard.tsv', 'wb') as tsvOut:
     tsvIn = csv.reader(tsvIn, delimiter='\t')
 
     arreglo = []
 
     for indice, compuesto in enumerate(tsvIn):
-        if (indice != 0):
-            arreglo.append(compuesto[3])
+        if indice != 0:
+            arreglo.append([compuesto[1], compuesto[3]])
 
+    tsvOut.write("compound_a\tcompound_b\tvalue\n")
 
     for i in range(len(arreglo)):
-        for j in range(i, len(arreglo)):
-            print "###"+str(i+1)+": "+str(arreglo[i])+" ###"+str(j+1)+": "+str(arreglo[j])
+        for j in range(i+1, len(arreglo)):
+            tsvOut.write(arreglo[i][0]+"\t"+arreglo[j][0]+"\t"+str(0.0)+"\n")
