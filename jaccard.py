30d8bdb6195c11cdc29ace16ada3b6e191d76f98
Alexander Olmedo <olmedo2408@gmail.com>
2017-11-10 23:51:48 -0500

    Calcular coeficiente de Jaccard y escribir a archivo

--- jaccard.py
+++ jaccard.py
@@ -10,10 +10,24 @@ with open('ZINC_chemicals.tsv','rb') as tsvIn, open('solution_jaccard.tsv', 'wb'
         if indice != 0:
             arreglo.append([compuesto[1], collections.Counter(compuesto[3])])
 
-    tsvOut.write("compound_a\tcompound_b\tvalue\n")
 
-    # for i in range(len(arreglo)):
-    #     for j in range(i+1, len(arreglo)):
-    #          print arreglo[i]
-    #
-    #         tsvOut.write(arreglo[i][0]+"\t"+arreglo[j][0]+"\t"+str(0.0)+"\n")
+    for i in range(len(arreglo)):
+        numElementos = 0
+        # Remover simbolos y otros caracteres
+        for simbolo in ['[',']','(',')','-','+','=','/']:
+            del arreglo[i][1][simbolo]
+
+        # Agregar el numero total de elementos
+        for caracter in arreglo[i][1]:
+            numElementos+=arreglo[i][1][caracter]
+        arreglo[i].append(numElementos)
+
+    for i in range(len(arreglo)):
+        for j in range(i+1, len(arreglo)):
+            elementosComunes = 0
+            for caracter in arreglo[i][1]:
+                if (arreglo[j][1].has_key(caracter)):
+                    elementosComunes += min (arreglo[i][1][caracter],arreglo[j][1][caracter])
+            tsvOut.write(arreglo[i][0] + "\t" + arreglo[j][0] + "\t" + "{0:.2f}".format(float(elementosComunes) / (arreglo[i][2]+arreglo[j][2] - elementosComunes)) + "\n")
+
+            print "{0:.2f}".format(float(elementosComunes) / (arreglo[i][2]+arreglo[j][2] - elementosComunes))
