58a22e5d2f57c72984c42a817c4ab3c9847b3169
Alexander Olmedo <olmedo2408@gmail.com>
2017-11-10 22:27:58 -0500

    Contar numero de instancias de cada caracter

--- jaccard.py
+++ jaccard.py
@@ -1,4 +1,5 @@
 import csv
+import collections
 
 with open('ZINC_chemicals.tsv','rb') as tsvIn, open('solution_jaccard.tsv', 'wb') as tsvOut:
     tsvIn = csv.reader(tsvIn, delimiter='\t')
@@ -7,10 +8,12 @@ with open('ZINC_chemicals.tsv','rb') as tsvIn, open('solution_jaccard.tsv', 'wb'
 
     for indice, compuesto in enumerate(tsvIn):
         if indice != 0:
-            arreglo.append([compuesto[1], compuesto[3]])
+            arreglo.append([compuesto[1], collections.Counter(compuesto[3])])
 
     tsvOut.write("compound_a\tcompound_b\tvalue\n")
 
-    for i in range(len(arreglo)):
-        for j in range(i+1, len(arreglo)):
-            tsvOut.write(arreglo[i][0]+"\t"+arreglo[j][0]+"\t"+str(0.0)+"\n")
+    # for i in range(len(arreglo)):
+    #     for j in range(i+1, len(arreglo)):
+    #          print arreglo[i]
+    #
+    #         tsvOut.write(arreglo[i][0]+"\t"+arreglo[j][0]+"\t"+str(0.0)+"\n")
