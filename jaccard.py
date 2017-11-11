e766bc1a59ed094ee6e53cd8610e5c70f381baea
Alexander Olmedo <olmedo2408@gmail.com>
2017-11-10 20:41:31 -0500

    Imprimir todas las combinaciones de compuestos

--- jaccard.py
+++ jaccard.py
@@ -1,8 +1,15 @@
 import csv
 
-with open('ZINC_chemicals.tsv','rb') as tsvIn:
+with open('ZINC_chemicals_test.tsv','rb') as tsvIn:
     tsvIn = csv.reader(tsvIn, delimiter='\t')
 
-    for indice, item in enumerate(tsvIn):
+    arreglo = []
+
+    for indice, compuesto in enumerate(tsvIn):
         if (indice != 0):
-            print item[3]
+            arreglo.append(compuesto[3])
+
+
+    for i in range(len(arreglo)):
+        for j in range(i, len(arreglo)):
+            print "###"+str(i+1)+": "+str(arreglo[i])+" ###"+str(j+1)+": "+str(arreglo[j])
