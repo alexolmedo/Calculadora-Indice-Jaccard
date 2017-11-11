e756a1592ddc432ac6d67e20f4973553d7e353c5
Alexander Olmedo <olmedo2408@gmail.com>
2017-11-11 13:23:53 -0500

    Añadido comentarios, cambiado nombres de variables

--- jaccard.py
+++ jaccard.py
@@ -1,32 +1,38 @@
 import csv
 import collections
 
-with open('ZINC_chemicals.tsv','rb') as tsvIn, open('solution_jaccard.tsv', 'wb') as tsvOut:
+with open('ZINC_chemicals.tsv','rb') as tsvIn, open('solution_python.tsv', 'wb') as tsvOut:
     tsvIn = csv.reader(tsvIn, delimiter='\t')
 
-    arreglo = []
+    arregloCompuestos = []
 
     for indice, compuesto in enumerate(tsvIn):
+        # Ignorar primera linea del archivo, agregar diccionario con la frecuencia de todos los caracteres al arreglo
         if indice != 0:
-            arreglo.append([compuesto[1], collections.Counter(compuesto[3])])
+            arregloCompuestos.append([compuesto[1], collections.Counter(compuesto[3])])
 
+    for i in range(len(arregloCompuestos)):
 
-    for i in range(len(arreglo)):
+        # Remover simbolos y otros caracteres del diccionario
+        for simbolo in ['[', ']', '(', ')', '-', '+', '=', '/']:
+            del arregloCompuestos[i][1][simbolo]
+
+        # Agregar el numero total de elementos al arreglo
         numElementos = 0
-        # Remover simbolos y otros caracteres
-        for simbolo in ['[',']','(',')','-','+','=','/']:
-            del arreglo[i][1][simbolo]
+        for caracter in arregloCompuestos[i][1]:
+            numElementos += arregloCompuestos[i][1][caracter]
+        arregloCompuestos[i].append(numElementos)
 
-        # Agregar el numero total de elementos
-        for caracter in arreglo[i][1]:
-            numElementos+=arreglo[i][1][caracter]
-        arreglo[i].append(numElementos)
+    # Escribir etiquetas de cada columna
+    tsvOut.write("compound_a\tcompound_b\tvalue\n")
 
-    for i in range(len(arreglo)):
-        for j in range(i+1, len(arreglo)):
+    for i in range(len(arregloCompuestos)):
+        for j in range(i+1, len(arregloCompuestos)):
             elementosComunes = 0
-            for caracter in arreglo[i][1]:
-                if (arreglo[j][1].has_key(caracter)):
-                    elementosComunes += min (arreglo[i][1][caracter],arreglo[j][1][caracter])
-            tsvOut.write(arreglo[i][0] + "\t" + arreglo[j][0] + "\t" + "{0:.2f}".format(float(elementosComunes) / (arreglo[i][2]+arreglo[j][2] - elementosComunes)) + "\n")
+            for caracter in arregloCompuestos[i][1]:
+                if (arregloCompuestos[j][1].has_key(caracter)):
+                    elementosComunes += min (arregloCompuestos[i][1][caracter], arregloCompuestos[j][1][caracter])
+            # Calculo y escritura a archivo del indice de Jaccard
+            indiceJaccard = float(elementosComunes) / (arregloCompuestos[i][2] + arregloCompuestos[j][2] - elementosComunes)
+            tsvOut.write(arregloCompuestos[i][0] + "\t" + arregloCompuestos[j][0] + "\t" + "{0:.2f}".format(indiceJaccard) + "\n")
 
