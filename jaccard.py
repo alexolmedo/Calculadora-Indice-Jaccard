34856701d30886e707b8e18a882b6afc9e432255
alexander <olmedo2408@gmail.com>
2017-11-11 22:51:17 -0500

    Calculo paralelizado

--- jaccard.py
+++ jaccard.py
@@ -1,9 +1,25 @@
 import csv
 import collections
+import multiprocessing
 
-with open('ZINC_chemicals.tsv','rb') as tsvIn, open('solution_python.tsv', 'wb') as tsvOut:
+
+def calculojaccard (hilo, numHilos):
+    global arregloCompuestos
+    for i in range(hilo, len(arregloCompuestos), numHilos):
+        for j in range(i+1, len(arregloCompuestos)):
+            elementosComunes = 0
+            for caracter in arregloCompuestos[i][1]:
+                if (arregloCompuestos[j][1].has_key(caracter)):
+                    elementosComunes += min (arregloCompuestos[i][1][caracter], arregloCompuestos[j][1][caracter])
+            # Calculo y escritura a archivo del indice de Jaccard
+            indiceJaccard = float(elementosComunes) / (arregloCompuestos[i][2] + arregloCompuestos[j][2] - elementosComunes)
+            with open('solution_python.tsv', 'ab') as tsvOut:
+                tsvOut.write(arregloCompuestos[i][0] + "\t" + arregloCompuestos[j][0] + "\t" + "{0:.2f}".format(indiceJaccard) + "\n")
+
+
+with open('ZINC_chemicals.tsv','rb') as tsvIn:
     tsvIn = csv.reader(tsvIn, delimiter='\t')
-
+    global arregloCompuestos
     arregloCompuestos = []
 
     for indice, compuesto in enumerate(tsvIn):
@@ -24,15 +40,16 @@ with open('ZINC_chemicals.tsv','rb') as tsvIn, open('solution_python.tsv', 'wb')
         arregloCompuestos[i].append(numElementos)
 
     # Escribir etiquetas de cada columna
-    tsvOut.write("compound_a\tcompound_b\tvalue\n")
+    with open('solution_python.tsv', 'wb') as tsvOut:
+        tsvOut.write("compound_a\tcompound_b\tvalue\n")
 
-    for i in range(len(arregloCompuestos)):
-        for j in range(i+1, len(arregloCompuestos)):
-            elementosComunes = 0
-            for caracter in arregloCompuestos[i][1]:
-                if (arregloCompuestos[j][1].has_key(caracter)):
-                    elementosComunes += min (arregloCompuestos[i][1][caracter], arregloCompuestos[j][1][caracter])
-            # Calculo y escritura a archivo del indice de Jaccard
-            indiceJaccard = float(elementosComunes) / (arregloCompuestos[i][2] + arregloCompuestos[j][2] - elementosComunes)
-            tsvOut.write(arregloCompuestos[i][0] + "\t" + arregloCompuestos[j][0] + "\t" + "{0:.2f}".format(indiceJaccard) + "\n")
+    nucleos = multiprocessing.cpu_count()
+    procesos = []
 
+    for nucleo in range(nucleos):
+        proceso = multiprocessing.Process(target=calculojaccard, args=(nucleo, nucleos))
+        procesos.append(proceso)
+        procesos[-1].start()
+
+    for proceso in procesos:
+        proceso.join()
