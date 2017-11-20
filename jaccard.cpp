ff8693af07dad020afcdbf5a8b8dd53a58ea5255
alexander <olmedo2408@gmail.com>
2017-11-19 20:24:21 -0500

    Leer .tsv y guardar en un vector

--- jaccard.cpp
+++ jaccard.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
+
 using namespace std;
 
 int main() 
 {
-    cout << "Hello, World!";
-    return 0;
+    typedef vector<vector<string> > Compuestos;
+    Compuestos compuestos;
+    ifstream input("ZINC_chemicals_test.tsv");
+    char const limite_linea = '\n';
+    char const limite_campo = '\t';
+    for (string linea; getline(input, linea, limite_linea); ) {
+        compuestos.push_back(Compuestos::value_type());
+        istringstream ss(linea);
+        for (string campo; getline(ss, linea, limite_campo); ) {
+            cout << linea << endl;
+            compuestos.back().push_back(linea);
+        }
+    }
 }
